b236a2c29d6d67584c06291816ef44ab56461217
Andres Yebra <andres.yebra@mercadolibre.com>
2020-11-01 19:29:29 -0300

    table tests added

---
+++ testing101/sum_test.go
@@ -0,0 +1,30 @@
+package testing101
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSum(t *testing.T) {
+	numbers := []int{1, 2, 3, 4, 5}
+	expected := 15
+	actual := Sum(numbers)
+
+	if actual != expected {
+		t.Errorf("Expected the sum of %v to be %d but instead got %d!", numbers, expected, actual)
+	}
+}
+
+func TestMultipleTestsForSum(t *testing.T) {
+	t.Run("All positive", testSumFunc([]int{1, 2, 3, 4, 5}, 15))
+	t.Run("Some Negative", testSumFunc([]int{1, -2, 3, -4, 2}, 3))
+}
+
+func testSumFunc(numbers []int, expected int) func(*testing.T) {
+	return func(t *testing.T) {
+		actual := Sum(numbers)
+		assert.Equal(t, expected, actual, fmt.Sprintf("Values expected to match [expected: %v] - [actual: %v]", expected, actual))
+	}
+}
