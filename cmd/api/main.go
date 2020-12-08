c585ad179a871595a690bcb9a548b3a31049e644
Andres Yebra <andres.yebra@mercadolibre.com>
2020-12-08 11:03:57 -0300

    This is the plain example from Jon, but this runs into a deadlock

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,17 +3,21 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
 func notify(services ...string) {
+	var wg sync.WaitGroup
 	for _, service := range services {
+		wg.Add(1)
 		go func(s string) {
 			fmt.Printf("Starting to notifing %s...\n", s)
 			time.Sleep(time.Duration(rand.Intn(3)) * time.Second)
 			fmt.Printf("Finished notifying %s...\n", s)
 		}(service)
 	}
+	wg.Wait()
 	fmt.Println("All services notified!")
 }
 
