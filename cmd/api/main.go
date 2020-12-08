db193d296c8195a33eb0a04e0e4a451bfc960352
Andres Yebra <andres.yebra@mercadolibre.com>
2020-12-08 11:06:40 -0300

    Now we use channels instead of waitGroups

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,22 +3,26 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"sync"
 	"time"
 )
 
 func notify(services ...string) {
-	var wg sync.WaitGroup
+	res := make(chan string)
+	count := 0
+
 	for _, service := range services {
-		wg.Add(1)
+		count++
 		go func(s string) {
 			fmt.Printf("Starting to notifing %s...\n", s)
 			time.Sleep(time.Duration(rand.Intn(3)) * time.Second)
-			fmt.Printf("Finished notifying %s...\n", s)
-			wg.Done()
+			res <- fmt.Sprintf("Finished %s", s)
 		}(service)
 	}
-	wg.Wait()
+
+	for i := 0; i < count; i++ {
+		fmt.Println(<-res)
+	}
+
 	fmt.Println("All services notified!")
 }
 
