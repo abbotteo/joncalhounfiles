b023056dcad93efebf9280660b288297f6df5b74
Andres Yebra <andres.yebra@mercadolibre.com>
2020-12-08 11:05:07 -0300

    Last commit is wrong, I've forgot to add the 'done()', thus the deadlock, Jon was right

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -15,6 +15,7 @@ func notify(services ...string) {
 			fmt.Printf("Starting to notifing %s...\n", s)
 			time.Sleep(time.Duration(rand.Intn(3)) * time.Second)
 			fmt.Printf("Finished notifying %s...\n", s)
+			wg.Done()
 		}(service)
 	}
 	wg.Wait()
