e58841c04808bcbd16214bc4aadaa10e65b2c2c2
Andres Yebra <andres.yebra@mercadolibre.com>
2020-12-08 11:01:27 -0300

    As it is, this doesn't waits for the goroutines to run

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
+func notify(services ...string) {
+	for _, service := range services {
+		go func(s string) {
+			fmt.Printf("Starting to notifing %s...\n", s)
+			time.Sleep(time.Duration(rand.Intn(3)) * time.Second)
+			fmt.Printf("Finished notifying %s...\n", s)
+		}(service)
+	}
+	fmt.Println("All services notified!")
+}
+
 func main() {
-	fmt.Print("The executable\n")
+	notify("Service-1", "Service-2", "Service-3")
+	// Running this outputs "All services notified!" but we
+	// won't see any of the services outputting their finished messages!
 }
