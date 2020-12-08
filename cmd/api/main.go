88d296a9e339701ad6c0daab96017e43235a0e22
Andres Yebra <andres.yebra@mercadolibre.com>
2020-12-08 11:24:25 -0300

    In this case is a mix, just get the first response and continue, use a waitgroupe to ensure all goroutines finish, also channel must have a buffer size to allow unused routines to write on

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,31 +3,36 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"runtime"
+	"sync"
 	"time"
 )
 
-func notify(services ...string) {
-	res := make(chan string)
+func notify(wg *sync.WaitGroup, services ...string) {
+	res := make(chan string, len(services))
+	wg.Add(len(services))
 	count := 0
-
 	for _, service := range services {
 		count++
 		go func(s string) {
 			fmt.Printf("Starting to notifing %s...\n", s)
 			time.Sleep(time.Duration(rand.Intn(3)) * time.Second)
 			res <- fmt.Sprintf("Finished %s", s)
+			wg.Done()
 		}(service)
 	}
 
-	for i := 0; i < count; i++ {
-		fmt.Println(<-res)
-	}
-
-	fmt.Println("All services notified!")
+	response := <-res
+	fmt.Println(response)
+	fmt.Println("One service notified!")
 }
 
 func main() {
-	notify("Service-1", "Service-2", "Service-3")
+	var wg sync.WaitGroup
+	notify(&wg, "Service-1", "Service-2", "Service-3", "Service-1b", "Service-2b", "Service-3b", "Service-1c", "Service-2c", "Service-3c")
 	// Running this outputs "All services notified!" but we
 	// won't see any of the services outputting their finished messages!
+	fmt.Printf("routines %v...\n", runtime.NumGoroutine())
+	wg.Wait()
+	fmt.Printf("routines %v...\n", runtime.NumGoroutine())
 }
