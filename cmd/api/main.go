35e2510ad91a1bb53437c2b5e72fcf9b635c6d62
Andres Yebra <andres.yebra@mercadolibre.com>
2021-01-12 12:20:02 -0300

    playing results

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,4 +9,10 @@ func main() {
 	This comment will remain as commit 'one'
 	 */
 	fmt.Print("The executable\n")
+	/*
+		this comment will be added to develop, then reverted, then
+		this branch will be rebased and check if the change is lost
+		The change was lost, so I move the pointer to 0bc89c99 which had
+		the comment. But I don't know if I have the changes in develop
+	*/
 }
