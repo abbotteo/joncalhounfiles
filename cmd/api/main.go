bcf7cbb020c1fbb7405a2aacfd9a3acf536fb21f
Andres Yebra <andres.yebra@mercadolibre.com>
2021-01-12 11:56:27 -0300

    Revert "commit two"

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,8 +9,4 @@ func main() {
 	This comment will remain as commit 'one'
 	 */
 	fmt.Print("The executable\n")
-	/*
-		this comment will be added to develop, then reverted, then
-		this branch will be rebased and check if the change is lost
-	*/
 }
