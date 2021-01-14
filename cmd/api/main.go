bb6653ed7b31d0995dc5d6aa3f70c199c57f47e3
Andres Yebra <andres.yebra@mercadolibre.com>
2021-01-13 21:07:54 -0300

    hitting various endpoints

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 )
 
 func main() {
-	/*
-	This comment will remain as commit 'one'
-	 */
-	fmt.Print("The executable\n")
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(j int) {
+			defer wg.Done()
+			time.Sleep(1 * time.Second)
+			fmt.Printf("Goroutine #%d finished.\n", j)
+		}(i)
+	}
+	wg.Wait()
 }
