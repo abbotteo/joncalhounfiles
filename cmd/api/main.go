db35238bc8313b1d36102bfed75e1784746decb5
Andres Yebra <andres.yebra@mercadolibre.com>
2021-01-13 21:36:45 -0300

    fourth step, my flavor

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,12 +8,14 @@ import (
 
 func main() {
 	urls := []string{
+		"https://invalid.gophercises.com",
 		"https://www.usegolang.com",
 		"https://testwithgo.com",
-		"https://invalid.gophercises.com",
+		"https://www.abbotteo.com.ar",
 	}
 	var wg sync.WaitGroup
 	responses := make([]string, len(urls))
+	racingOrder := 0
 
 	for i, url := range urls {
 		wg.Add(1)
@@ -25,12 +27,17 @@ func main() {
 		go func(j int, url string) {
 			defer wg.Done()
 			resp, err := http.Get(url)
+			racingOrder++
 			if err != nil {
-				responses[j] = fmt.Sprintf("error: %v", err)
+				responses[j] = fmt.Sprintf("error: %v - order: %v", err, racingOrder)
 				return
 			}
-			responses[j] = resp.Status
+			responses[j] = fmt.Sprintf("%v - order: %v", resp.Status, racingOrder)
 		}(i, url)
 	}
 	wg.Wait()
+
+	for i, url := range urls {
+		fmt.Printf("%v: %v\n", url, responses[i])
+	}
 }
